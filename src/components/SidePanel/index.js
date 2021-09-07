a0389c19fb9b6d2a10ac7906aafe1a3206cec623
Jose Arias <developstuff93@gmail.com>
2021-09-07 15:20:43 -0400

    add SidePanel component

---
+++ src/components/SidePanel/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import useAppState from "../../hooks/useAppState";
+import AddNewIcon from "../../icons/AddNewIcon";
+import CaretRightIcon from "../../icons/CaretRightIcon";
+import "./style.css";
+
+export default function SidePanel() {
+  const { notes, selectedNote, setSelectedNote } = useAppState();
+
+  const handleSelectNote = (note) => {
+    setSelectedNote(note);
+  };
+
+  return (
+    <div className="side-panel">
+      <div className="side-panel_title">
+        <div className="title">ForethoughtNote</div>
+        <div className="add-new-icon">
+          <AddNewIcon />
+        </div>
+      </div>
+      {notes.map((note) => {
+        const { id, title, date_created } = note;
+        return (
+          <div
+            key={id}
+            className="side-panel_note"
+            onClick={() => handleSelectNote(note)}
+          >
+            <div className="note-details">
+              <span className="details-title">{title}</span>
+              <span className="details-date">{date_created}</span>
+            </div>
+            {id === selectedNote.id && (
+              <div className="note-action">
+                <CaretRightIcon />
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
